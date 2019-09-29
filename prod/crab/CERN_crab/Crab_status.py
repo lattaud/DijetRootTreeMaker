035036a3f55c6e8a8796c862cc9d6718cef5261b
Lattaud <hugueslattaud@sfr.fr>
2019-09-29 09:55:41 +0200

    add crab control script

---
+++ prod/crab/CERN_crab/Crab_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import argparse, os, tempfile, shutil, sys,math,pickle,itertools
+parent = os.path.dirname(os.getcwd())
+sys.path.append(parent)
+from subprocess import call, PIPE, STDOUT, Popen
+import argparse
+import datetime
+from ROOT import *
+
+DirList = ["crab_GJet-07Aug17-Run2016B_ver2_v1_NEW_EG_systa_dded_V1","crab_GJet-07Aug17-Run2016C_NEW_EG_systa_dded_V1","crab_GJet-07Aug17-Run2016D_NEW_EG_systa_dded_V1","crab_GJet-07Aug17-Run2016E_NEW_EG_systa_dded_V1","crab_GJet-07Aug17-Run2016Fearly_NEW_EG_systa_dded_V1","crab_GJet-07Aug17-Run2016Flate__NEW_EG_systa_dded_V1","crab_GJet-07Aug17-Run2016G_NEW_EG_systa_dded_V1","crab_GJet-07Aug17-Run2016H_NEW_EG_systa_dded_V1","crab_GJets_HT-100To200_NEW_EG_systa_dded_V1","crab_GJets_HT-100To200_ext_NEW_EG_systa_dded_V1","crab_GJets_HT-200To400_NEW_EG_systa_dded_V1","crab_GJets_HT-200To400_ext_NEW_EG_systa_dded_V1","crab_GJets_HT-400To600_NEW_EG_systa_dded_V1","crab_GJets_HT-400To600_ext_NEW_EG_systa_dded_V1","crab_GJets_HT-40To100_NEW_EG_systa_dded_V1","crab_GJets_HT-40To100_ext_NEW_EG_systa_dded_V1","crab_GJets_HT-600ToInf_NEW_EG_systa_dded_V1","crab_GJets_HT-600ToInf_ext_NEW_EG_systa_dded_V1","crab_G1Jets_Pt-50To100_NEW_EG_systa_dded_V1","crab_G1Jets_Pt-50To100_ext_NEW_EG_systa_dded_V1","crab_G1Jets_Pt-100To250_NEW_EG_systa_dded_V1","crab_G1Jets__Pt-100To250_ext_NEW_EG_systa_dded_V1","crab_G1Jets_Pt-100To250_ext2_NEW_EG_systa_dded_V1","crab_G1Jets__Pt-250To400_NEW_EG_systa_dded_V1","crab_G1Jets__Pt-250To400_ext_NEW_EG_systa_dded_V1","crab_G1Jets__Pt-250To400_ext2_NEW_EG_systa_dded_V1","crab_G1Jets__Pt-400To650_NEW_EG_systa_dded_V1","crab_G1Jets__Pt-400To650_ext_NEW_EG_systa_dded_V1","crab_G1Jets__Pt-650ToInf_NEW_EG_systa_dded_V1","crab_G1Jets__Pt-650ToInf_ext_NEW_EG_systa_dded_V1"]
+
+for iDir in DirList:
+        env = "source /cvmfs/cms.cern.ch/crab3/crab.sh && export SCRAM_ARCH=slc6_amd64_gcc530 "
+	cmd = "crab status "+iDir
+	print(cmd)
+	os.system(env + "&&" + cmd)
